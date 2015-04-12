6b86f30edd0246862d7cc8868725abd3ca4f8c76
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-12 11:47:44 -0600

    First test, not fully grokking how the table html works and getting error messages about variables not being used.

--- php_chpt_7_exer2.php
+++ php_chpt_7_exer2.php
@@ -2,9 +2,9 @@
 function activitytwo ($var1, $var2, $var3)
 	{
 		$var1 = $var1 +5;
-		$var2 = $var2++;
-		$var3 += 500;
+		$var2 = $var2 -5;
+		$var3 += 500 ;
 		echo "<table border=\"1\">\n";
-		echo "<tr><td>\$var3</td><td>$var1</td><td>$var2</td>";
+		echo "<tr><td>$var3</td><td>$var1</td><td>$var2</td>";
 	}
 activitytwo(10,1,20);
